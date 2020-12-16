07d9165529712513aa349ec61bccf6978ae9ea53
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-16 22:47:27 +0900

    study start

---
+++ 10.Array/3.systemArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.systemArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 30, 20, 7, 1 };
+            Console.WriteLine($"Type of array : {array.GetType()}");
+            Console.WriteLine($"Base type Of array : {array.GetType().BaseType}");
+        }
+    }
+}
