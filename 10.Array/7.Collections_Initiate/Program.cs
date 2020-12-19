a6f332f0326dc3bc95af998524d93b4a32a479be
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-19 11:51:53 +0900

    feat CollectionsInitiate

---
+++ 10.Array/7.Collections_Initiate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using static System.Console;
+
+namespace _7.Collections_Initiate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 123, 456, 789 };  // arr 초기화
+
+            ArrayList list = new ArrayList(arr);    // arr를 이용한 초기화
+            foreach (object obj in list)
+                WriteLine($"ArrayList : {obj}");    // 123 456 789
+            WriteLine();
+
+            Stack stack = new Stack(arr);   // arr를 이용한 초기화
+            foreach (object obj in stack)
+                WriteLine($"Stack : {obj}");    // 789 456 123 stack은 새로들어오는 인풋 값을 맨 앞에 할당하기 때문에 반대
+            WriteLine();
+
+            Queue queue = new Queue(arr);
+            foreach (object obj in queue)       // 123 456 789
+                WriteLine($"Queue : {obj}");
+            WriteLine();
+
+            ArrayList list2 = new ArrayList() { 11, 22, 33 };   
+            foreach (object obj in list2)
+                WriteLine($"ArrayList2 : {obj}");
+            
+            
+        }
+    }
+}
