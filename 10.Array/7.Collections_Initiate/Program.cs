42e18dd1d419e3a6ceead312beecbc3098a08bca
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-19 23:36:23 +0900

    feat Collections Initiate2

--- 10.Array/7.Collections_Initiate/Program.cs
+++ 10.Array/7.Collections_Initiate/Program.cs
@@ -32,8 +32,24 @@ namespace _7.Collections_Initiate
             ArrayList list2 = new ArrayList() { 11, 22, 33 };   
             foreach (object obj in list2)
                 WriteLine($"ArrayList2 : {obj}");
-            
-            
+
+            Hashtable ht = new Hashtable()  // HashTable 초기화 첫번째 방법
+            {
+                {"하나",1 },
+                {"둘", 2 },
+                {"셋", 3 },
+                {"넷", 4 },
+                {"다섯", 5 }
+            };
+
+            Hashtable ht2 = new Hashtable() // HashTable 초기화 두번째 방법
+            {
+                ["하나"] = 1,
+                ["둘"] = 2,
+                ["셋"] = 3,
+                ["넷"] = 4,
+                ["다섯"] = 5
+            };
         }
     }
 }
