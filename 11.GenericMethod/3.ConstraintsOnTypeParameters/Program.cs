92e6b6d11a68448f05add8c7a06d1af8d39b9130
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-29 14:18:16 +0900

    feat. GenericClass

---
+++ 11.GenericMethod/3.ConstraintsOnTypeParameters/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.ConstraintsOnTypeParameters
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+
+        public void CopyArray<T>(T[] Source) where T : U
+        {
+            Source.CopyTo(Array, 0);
+        }
+    }
+    class Program
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T();
+        }
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            
+            for (int i = 0; i < a.Array.Length; i++)
+            {
+                a.Array[i] = i;
+            }
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+
+               
+        }
+    }
+}
