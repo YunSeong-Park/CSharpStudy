2d4489fbf96dec2497ddb446fdbf6ef319581a06
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-29 23:04:49 +0900

    feat List<T>
    List<T>는 T에 int, string과 같은 형식을 지정해 주어야하는 번거로움이있다.
    
    Collection의 ArrayList는 따로 형식을 지정해주지 않아도 돼 편리하다.
    하지만, ArrayList는 모든 요소를 object로 관리해서 정보를 저장하고 열때 마다 추가적인 리소스가든다.

---
+++ 11.GenericMethod/4.ListT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.ListT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();   //  int 형식의 list를 생성
+
+            for (int i = 0; i < 5; i++) 
+                list.Add(i);        // 0 1 2 3 4
+
+            foreach (int item in list)
+                Console.Write($"{item} ");  // 출력
+            Console.WriteLine();
+
+            list.RemoveAt(2);   // 0 1 3 4
+
+            foreach (int item in list)
+                Console.Write($"{item} ");  // 출력
+            Console.WriteLine();
+
+            list.Insert(2, 2);  // 0 1 2 3 4
+
+            foreach (int item in list)
+                Console.Write($"{item} ");  // 출력
+            Console.WriteLine();
+
+        }
+    }
+}
