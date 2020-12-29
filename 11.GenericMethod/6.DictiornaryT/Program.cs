3eaf120cd1a2419070f6049d8a79225e210b2043
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2020-12-29 23:30:44 +0900

    feat DictionaryT

---
+++ 11.GenericMethod/6.DictiornaryT/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.DictiornaryT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();    // key는 string value는 int
+
+            dic["하나"] = 1;
+            dic["둘"] = 2;
+            dic["셋"] = 3;
+            dic["넷"] = 4;
+            dic["다섯"] = 5;
+
+            Console.WriteLine(dic["하나"]);  // 1
+            Console.WriteLine(dic["둘"]);    // 2
+            Console.WriteLine(dic["셋"]);    // 3
+            Console.WriteLine(dic["넷"]);    // 4
+            Console.WriteLine(dic["다섯"]);  // 5
+        }
+    }
+}
