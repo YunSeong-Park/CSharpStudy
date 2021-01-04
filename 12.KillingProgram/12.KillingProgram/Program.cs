b2383a64e2210e77958be80a3317bf338a60fdaf
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-01-04 22:13:13 +0900

    feat try catch

--- 12.KillingProgram/12.KillingProgram/Program.cs
+++ 12.KillingProgram/12.KillingProgram/Program.cs
@@ -11,11 +11,21 @@ namespace _12.KillingProgram
         static void Main(string[] args)
         {
             int[] arr = { 1, 2, 3 };
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(arr[i]);  //  i가 배열의 크기 -1을 넘어서면서 예외를 일으키고 종료된다. 
+            try     //  해당 블록에서 에러가 나오면 catch로 이동
+            { 
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);  //  i가 배열의 크기 -1을 넘어서면서 예외를 일으키고 종료된다. 
+                }
             }
-            Console.WriteLine("종료");    // 16행에서 예외를 일으키고 종료됐기 때문에 실행 되지 않는다.
+            catch( IndexOutOfRangeException e)  // IndexOutOfRangeException 발생시 해당 블록실행
+            {
+                Console.WriteLine($"예외가 발생했습니다. : {e.Message}");
+            }
+            // 종료 후 순차 실행
+
+            Console.WriteLine("종료");    // try catch 문으로 실행 가능
+            
         }
     }
 }
