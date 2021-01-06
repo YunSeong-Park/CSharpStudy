2954adda1a3a84c6ab3b74c8cf687571ebe81035
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-01-07 00:00:05 +0900

    7.TryExample

---
+++ 12.KillingProgram/7. TryExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.TryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[10];
+
+                for (int i = 0; i < 10; i++)
+                    arr[i] = i;
+
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);  // arr 범위에 벗어나 오류 발생
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램 종료");
+            }
+        }
+    }
+}
